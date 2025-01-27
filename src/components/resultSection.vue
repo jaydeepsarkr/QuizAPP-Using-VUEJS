75a318e667e85cdcf2368097dea1b5b1fade8d4f
Jaydeep Sarkar <jaydeepsarkr@gmail.com>
2025-01-27 11:07:49 +0530

    completed the quiz project

---
+++ src/components/resultSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="result">
+        <div class="title">{{ results[resultINDEX].title }}</div>
+        <div class="desc">
+           {{ results[resultINDEX].desc }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props:["totalCorrect","results"],
+    computed: {
+        resultINDEX(){
+             let index = 0;
+
+             this.results.forEach((e,i)=>{
+                if(e.min<=this.totalCorrect&&e.max>=this.totalCorrect){
+                    index = i;
+
+                }
+             })
+             return index;
+        }
+
+
+    }
+
+
+}
+</script>
